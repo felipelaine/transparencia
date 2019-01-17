5479f003539604a7e627db35a068a2cd06ac7e24
DESKTOP-E0T0K18\Felipe <felipe@felipe.com.br>
2019-01-17 14:06:07 -0200

    dash

--- src/app.py
+++ src/app.py
@@ -6,36 +6,92 @@ from src.config import Config, DevelopmentConfig
 from src.models.despesas.despesa import Despesas
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
 
-app_dash = dash.Dash(__name__, server=app)
-app_dash.config.requests_pathname_prefix = ''
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'c78bf172206ce24f77d6363a2d754b59/raw/'
+    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
+    'usa-agricultural-exports-2011.csv')
 
-app_dash.layout = html.Div([
-    html.H1('Dash application'),
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+df = pd.read_sql_query("""
+    SELECT ds_funcao_governo, sum(cast(replace(vl_despesa,',','.') as float)) vl_despesa
+    FROM despesas
+    WHERE ds_municipio = 'Sertгozinho' AND
+           mes_referencia = '4'
+    GROUP BY ds_funcao_governo
+    """, con=engine)
+
+app_dash = dash.Dash(__name__, server=app, external_stylesheets=external_stylesheets)
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app_dash.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
     dcc.Graph(
-        id='basic-graph',
+        id='example-graph-2',
         figure={
-            'data':[
-                {
-                    'x': [0, 1],
-                    'y': [0, 1],
-                    'type': 'line'
-                }
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
-                'title': 'Basic Graph'
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
             }
         }
-    )
+    ),
+
+    html.Div(children=[
+        html.H4(children='US Agriculture Exports (2011)'),
+        generate_table(df)
+    ])
 ])
 
 manager = APIManager(flask_sqlalchemy_db=db)
 manager.init_app(app)
 manager.create_api(Despesas, app=app, results_per_page=2500)
+
 
 @app.route('/')
 def home():
@@ -43,10 +99,12 @@ def home():
     despesas = Despesas.query.filter().limit(100).all()
     return render_template('home.html', despesas=despesas)
 
+
 @app.route('/dash')
 def dash():
     print("dash")
     return app_dash
+
 
 @app.route('/despesas_funcao')
 def despesas_funcao():
@@ -59,6 +117,7 @@ def despesas_funcao():
     """
     despesas = db.engine.execute(sql_command)
 
+
     d, despesas_list = {}, []
     for row in despesas:
         for column in row.items():
@@ -67,5 +126,6 @@ def despesas_funcao():
 
     return render_template('dashboards/despesa_funcao.html', despesas=despesas_list)
 
+
 if __name__ == "__main__":
-    app.run(debug=DevelopmentConfig.DEBUG)
+    app.run(debug=True)
